a9278a556ce23f0e1e03d7bf3a6b8d3da967a581
Samia <koukii.samia@gmail.com>
2023-07-18 02:00:51 +0400

    Initial Commit

---
+++ gittHubDemoUpload/gittHubDemoUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittHubDemoUploadApp.swift
+//  gittHubDemoUpload
+//
+//  Created by APPLE on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct gittHubDemoUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
