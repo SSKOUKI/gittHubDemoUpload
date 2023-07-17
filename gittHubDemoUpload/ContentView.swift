8f678d98350935f53177e454ce29a3ca01836e31
Samia <koukii.samia@gmail.com>
2023-07-18 02:22:52 +0400

    Make some visual changes in our text

--- gittHubDemoUpload/ContentView.swift
+++ gittHubDemoUpload/ContentView.swift
@@ -14,8 +14,11 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text ("This is great")
         }
+        .font(.system(size: 100))
         .padding()
+        .foregroundColor(.brown)
     }
 }
 
